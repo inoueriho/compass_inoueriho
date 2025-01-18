703021ddad1909aeecd6ef68f1dfff9b0f6440d4
井上理歩 <>
2025-01-18 15:58:05 +0900

    登録しました。

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Users\Subjects;
+use Illuminate\Support\Facades\DB;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -13,5 +14,13 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            ['id' => '1',
+            'subject' => '国語'],
+            ['id' => '2',
+            'subject' => '数学'],
+            ['id' => '3',
+            'subject' => '英語']
+        ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Users\User;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,42 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+         DB::table('users')->insert([
+            [
+              'over_name' => 'Atlas',
+              'under_name' => 'オレンジ',
+              'over_name_kana' => 'アトラス',
+              'under_name_kana' => 'オレンジ',
+              'mail_address' => '678910@mail',
+              'sex' => '1',
+              'birth_day' => '2000.6.21',
+              'role' => '4',
+              'password' => '62775115',
+            ],
 
+            [
+              'over_name' => 'Atlas',
+              'under_name' => '緑',
+              'over_name_kana' => 'アトラス',
+              'under_name_kana' => 'ミドリ',
+              'mail_address' => '7891011@mail',
+              'sex' => '1',
+              'birth_day' => '2000.11.27',
+              'role' => '4',
+              'password' => '751151127',
+            ],
+            [
+              'over_name' => 'Atlas',
+              'under_name' => '黒',
+              'over_name_kana' => 'アトラス',
+              'under_name_kana' => 'クロ',
+              'mail_address' => '89101112@mail',
+              'sex' => '1',
+              'birth_day' => '2000.2.16',
+              'role' => '4',
+              'password' => '1151127216',
+             ]
+
+        ]);
     }
 }
