b18f9c9f8c8e3edc5f7ad092b648eaed345c4f90
井上理歩 <>
2025-02-18 21:33:12 +0900

    ログイン画面に入れるようになりました。

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -24,20 +24,20 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'over_name' => ' required | string | max:10',
-            'under_name' => ' required | string | max:10',
-            'over_name_kana' => ' required | string | max:30 | /^[ｧ-ﾝﾞﾟ]*$/ ',
-            'under_name_kana' => ' required | string | max:30 | /^[ｧ-ﾝﾞﾟ]*$/ ',
-            'mail_address' => 'required |email | min:5 | max:100 | unique:users,mail',
-            'sex' => 'required | regex:/^[男|女|その他]+$/',
-            'old_year' => 'required | date | after:2000-01-01 |',
-            'old_month' => 'required | date | after:2000-01-01 |',
-            'old_day' => 'required | date | after:2000-01-01 |',
-            'role' => 'required | regex:/^[講師(国語)|講師(数学)|講師(英語)|生徒]+$/ |',
-            'password' => ' required | alpha_dash | min:8 | max:30 | confirmed',
-            'password_confirmation' => ' required |string | alpha_num | min:8 | max:20',
-            //
-        ];
+        // return [
+        //     'over_name' => ' required | string | max:10',
+        //     'under_name' => ' required | string | max:10',
+        //     'over_name_kana' => ' required | string | max:30 | /^[ｧ-ﾝﾞﾟ]*$/ ',
+        //     'under_name_kana' => ' required | string | max:30 | /^[ｧ-ﾝﾞﾟ]*$/ ',
+        //     'mail_address' => 'required |email | min:5 | max:100 | unique:users,mail',
+        //     'sex' => 'required | regex:/^[男|女|その他]+$/',
+        //     'old_year' => 'required | date | after:2000-01-01 |',
+        //     'old_month' => 'required | date | after:2000-01-01 |',
+        //     'old_day' => 'required | date | after:2000-01-01 |',
+        //     'role' => 'required | regex:/^[講師(国語)|講師(数学)|講師(英語)|生徒]+$/ |',
+        //     'password' => ' required | alpha_dash | min:8 | max:30 | confirmed',
+        //     'password_confirmation' => ' required |string | alpha_num | min:8 | max:20',
+        //     //
+        // ];
     }
 }
