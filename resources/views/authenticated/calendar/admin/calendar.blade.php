c79a9b412304141d13dc65ff2c2b2c5736e84068
井上理歩 <>
2025-02-22 15:32:54 +0900

    修正しました。

--- resources/views/authenticated/calendar/admin/calendar.blade.php
+++ resources/views/authenticated/calendar/admin/calendar.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.sidebar')
 
 @section('content')
+@can ('admin_only')
 <div class="w-75 m-auto">
   <div class="w-100">
     <p>{{ $calendar->getTitle() }}</p>
     <p>{!! $calendar->render() !!}</p>
   </div>
 </div>
+@endcan
 @endsection
--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.sidebar')
 
 @section('content')
+@can ('admin_only')
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
     <p><span>日</span><span class="ml-3">部</span></p>
@@ -18,4 +19,5 @@
     </div>
   </div>
 </div>
+@endcan
 @endsection
