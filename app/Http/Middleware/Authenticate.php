5fb658f6f339a3f6e9b2fccc37cd95e1aa92e0aa
井上理歩 <>
2025-03-03 21:20:18 +0900

    ログイン画面に戻るようになりました。

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('top.show');
+            return route('loginView');
         }
     }
 }
